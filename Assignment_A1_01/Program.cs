446738125d8fece22c27743cbf87f1d7889fa775
Linda Forslund <89069935+Lillypad92@users.noreply.github.com>
2024-01-08 13:04:59 +0100

    Initial commit

---
+++ Assignment_A1_01/Program.cs
@@ -0,0 +1,35 @@
+﻿using Assignment_A1_01.Models;
+using Assignment_A1_01.Services;
+
+namespace Assignment_A1_01
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            double latitude = 60.61667;
+            double longitude = 16.76667;
+
+            Forecast forecast = await new OpenWeatherService().GetForecastAsync(latitude, longitude);
+
+            //Your Code to present each forecast item in a grouped list
+            Console.WriteLine($"Weather forecast for {forecast.City}");
+
+            var groupedByDates = forecast.Items.GroupBy(x => x.DateTime.DayOfYear);
+
+            foreach (var dates in groupedByDates) 
+            {
+
+                DateTime dateTimeOfYear = new DateTime(DateTime.Now.Year, 1, 1).AddDays(dates.Key - 1);
+              
+                Console.WriteLine(dateTimeOfYear.ToString("yyyy-MM-dd"));
+                foreach (var date in dates) 
+                {
+                    Console.WriteLine($"- {date.DateTime.ToString("H:mm")}: {date.Description}, temperatur {date.Temperature} Celsius, wind: {date.WindSpeed} m/s");
+                    
+                }
+            }
+
+        }
+    }
+}
---
+++ Assignment_A1_01/Services/OpenWeatherService.cs
@@ -0,0 +1,60 @@
+﻿using System.Net.Http.Json;
+
+using Assignment_A1_01.Models;
+
+namespace Assignment_A1_01.Services
+{
+    public class OpenWeatherService
+    {
+        HttpClient httpClient = new HttpClient();
+
+        // Your API Key
+        readonly string apiKey = "6cd8834c2f5e1aba8affbdf8bb70b384"; 
+
+        public async Task<Forecast> GetForecastAsync(double latitude, double longitude)
+        {
+            //https://openweathermap.org/current
+            var language = System.Globalization.CultureInfo.CurrentUICulture.TwoLetterISOLanguageName;
+            var uri = $"https://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&units=metric&lang={language}&appid={apiKey}";
+
+            //Read the response from the WebApi
+            HttpResponseMessage response = await httpClient.GetAsync(uri);
+            response.EnsureSuccessStatusCode();
+            WeatherApiData wd = await response.Content.ReadFromJsonAsync<WeatherApiData>();
+
+            //Convert WeatherApiData to Forecast using Linq.
+            //Your code            
+            var forecast = new Forecast();
+
+            forecast.City = wd.city.name;
+
+            forecast.Items = new List<ForecastItem>();
+
+            forecast.Items = wd.list.Select(x => new ForecastItem()
+            {
+                DateTime = UnixTimeStampToDateTime(x.dt),
+                Temperature = x.main.temp,
+                WindSpeed = x.wind.speed,
+                Description = x.weather[0].description,
+                Icon = $"http://openweathermap.org/img/w/{x.weather.First().icon}.png"
+            }).ToList();
+
+            //Hint: you will find 
+            //City: wd.city.name
+            //Daily forecast in wd.list, in an item in the list
+            //      Date and time in Unix timestamp: dt 
+            //      Temperature: main.temp
+            //      WindSpeed: wind.speed
+            //      Description:  first item in weather[].description
+            //      Icon:  $"http://openweathermap.org/img/w/{wdle.weather.First().icon}.png"   //NOTE: Not necessary, only if you like to use an icon
+
+            return forecast;
+        }
+        private DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dateTime;
+        }
+    }
+}
